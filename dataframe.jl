eb1d8298b17ca836ca51106c9eaebaf4170037a9
mack lee <114943402+macklee251@users.noreply.github.com>
2024-02-13 13:57:02 -0300

    first commit

---
+++ dataframe.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.36
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6c8cfb38-ca90-11ee-3501-efccbaec9ea9
+
+
+# ╔═╡ Cell order:
+# ╠═6c8cfb38-ca90-11ee-3501-efccbaec9ea9
